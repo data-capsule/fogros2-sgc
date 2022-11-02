1cc7ceda07a9529690dbda80ef08a26ab35f3fd4
Eric Chen <kych@berkeley.edu>
2022-11-01 17:01:13 -0700

    default gateway - like meta prefix in IP

--- core/src/rib.rs
+++ core/src/rib.rs
@@ -3,17 +3,23 @@
 extern crate multimap; 
 use multimap::MultiMap;
 use std::net::Ipv4Addr;
+use utils::app_config::AppConfig;
+use utils::conversion::str_to_ipv4;
 
 pub struct RoutingInformationBase {
     pub routing_table: MultiMap<Vec<u8>, Ipv4Addr>,  
+    pub default_route: Vec<Ipv4Addr>,
 } 
 
 
 impl RoutingInformationBase {
-    pub fn new() -> RoutingInformationBase {
+    pub fn new(config: &AppConfig) -> RoutingInformationBase {
+        // TODO: config can populate the RIB somehow 
+        let m_gateway_addr = str_to_ipv4(&config.ip_gateway);
 
         RoutingInformationBase{
             routing_table: MultiMap::new(),
+            default_route: Vec::from([m_gateway_addr])
         }
     }
 
@@ -22,7 +28,16 @@ impl RoutingInformationBase {
         Some(())
     }
 
-    pub fn get(&mut self, key: Vec<u8>) -> Option<&Vec<Ipv4Addr>> {
+    // rust doesn't support default param.
+    pub fn get_no_default(& self, key: Vec<u8>) -> Option<&Vec<Ipv4Addr>> {
         self.routing_table.get_vec(&key)
     }
+
+    pub fn get(&self, key: Vec<u8>)-> &Vec<Ipv4Addr> {
+        match self.get_no_default(key) {
+            Some(v) => v, 
+            None => &self.default_route
+        }
+    }
+    
 }
