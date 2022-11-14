153e08190b74da247c8a00338825b129190ec36b
Eric Chen <kych@berkeley.edu>
2022-11-14 15:07:23 -0800

    use bound for mutual communication

--- core/src/commands.rs
+++ core/src/commands.rs
@@ -9,32 +9,33 @@ use crate::crypto::cert::*;
 use futures::future; // 0.3.19
 use std::time::Duration;
 use tokio::{
-    sync::mpsc::{self, UnboundedSender},
+    sync::mpsc::{self, channel, Sender},
     time,
 }; // 1.16.1
 
 use tokio::net::{TcpListener, TcpStream};
 use std::io;
-async fn process(socket: TcpStream, foo_tx: &UnboundedSender<String>) {
+async fn process(socket: TcpStream, foo_tx: &Sender<String>) {
     // ...
     println!("got packets!!");
     let message = format!("hello");
-    foo_tx.send(message).unwrap();
+    foo_tx.send(message);
 }
 
-async fn tcp_listener(msg: &'static str, foo_tx: UnboundedSender<String>)  {
+async fn tcp_listener(msg: &'static str, foo_tx: Sender<String>)  {
     let listener = TcpListener::bind("127.0.0.1:9999").await.unwrap();
     loop {
         let (socket, _) = listener.accept().await.unwrap();
         // Process each socket concurrently.
+        // TODO: currently, it can only handle one concurrent session
         process(socket, &foo_tx).await
     }
 }
 
-async fn message_sender(msg: &'static str, foo_tx: UnboundedSender<String>) {
+async fn message_sender(msg: &'static str, foo_tx: Sender<String>) {
     for count in 0.. {
         let message = format!("{msg}{count}");
-        foo_tx.send(message).unwrap();
+        foo_tx.send(message);
 
         if msg == "foo" {
             time::sleep(Duration::from_millis(100)).await;
@@ -46,16 +47,16 @@ async fn message_sender(msg: &'static str, foo_tx: UnboundedSender<String>) {
 
 #[tokio::main]
 async fn router_async_loop() {
-    let (foo_tx, mut foo_rx) = mpsc::unbounded_channel();
-    let (bar_tx, mut bar_rx) = mpsc::unbounded_channel();
+    let (foo_tx, mut foo_rx) = mpsc::channel(32);
+    let (bar_tx, mut bar_rx) = mpsc::channel(32);
 
     let foo_sender_handle = tokio::spawn(message_sender("foo", foo_tx));
     //let bar_sender_handle = tokio::spawn(message_sender("bar", bar_tx));
     let bar_sender_handle = tokio::spawn(tcp_listener("bar", bar_tx));
 
     let receive_handle = tokio::spawn(async move {
-        let mut foo = None;
-        let mut bar = None;
+        let mut foo:Option<String> = None;
+        let mut bar:Option<String> = None;
 
         loop {
             tokio::select! {
