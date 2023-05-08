281c57b39a5154a407b902fd004a91bbc9a7e767
Eric Chen <kych@berkeley.edu>
2023-05-08 15:14:45 -0700

    use redis to query publisher and subscriber

--- core/src/topic_manager.rs
+++ core/src/topic_manager.rs
@@ -9,6 +9,7 @@ use crate::structs::{
 
 use async_datachannel::DataStream;
 use serde::{Deserialize, Serialize};
+use tracing::subscriber;
 use std::collections::HashMap;
 use tokio::select;
 
@@ -17,7 +18,7 @@ use tokio::sync::mpsc::{self};
 use tokio::time::{sleep, timeout};
 use tokio::time::Duration;
 use utils::app_config::AppConfig;
-use redis::{self, Client, Commands, PubSubCommands};
+use redis::{self, Client, Commands, PubSubCommands, RedisResult, transaction};
 use redis_async::{client, resp::FromResp};
 use futures::StreamExt;
 
@@ -56,6 +57,24 @@ async fn determine_topic_action(topic_name: String) -> String {
     }
 }
 
+// add a publisher/subscriber to the database
+fn add_entity_to_database_as_transaction(redis_url: &str, key: &str, value: &str) -> RedisResult<()> {
+    let client = Client::open(redis_url)?;
+    let mut con = client.get_connection()?;
+    let (new_val,) : (isize,) = redis::transaction(&mut con, &[key], |con, pipe| {
+        pipe.lpush(key, value).query(con)
+    })?;
+    println!("The incremented number is: {}", new_val);
+    Ok(())
+}
+
+// get list of publishers/subscribers from the database
+fn get_entity_from_database(redis_url: &str, key: &str) -> RedisResult<Vec<String>> {
+    let client = Client::open(redis_url)?;
+    let mut con = client.get_connection()?;
+    let list: Vec<String> = con.lrange(key, 0, -1)?;
+    Ok(list)
+}
 
 pub async fn ros_topic_creator(
     stream: async_datachannel::DataStream, node_name: String, topic_name: String,
@@ -93,52 +112,91 @@ pub async fn ros_topic_creator(
 }
 
 
+
 async fn create_new_remote_publisher(
     topic_gdp_name: GDPName, topic_name: String, topic_type: String, certificate: Vec<u8>,
 ) {
-    let webrtc_stream = register_webrtc_stream(gdp_name_to_string(topic_gdp_name), None).await;
-    info!("publisher registered webrtc stream");
-    let _ros_handle = ros_topic_creator(
-        webrtc_stream,
-        format!("{}_{}", "ros_manager_node", rand::random::<u32>()),
-        topic_name.clone(),
-        topic_type,
-        "sub".to_string(),
-        certificate.clone(),
-    )
-    .await;
+    let publisher_listening_gdp_name = generate_random_gdp_name();
+
+    // currently open another synchronous connection for put and get
+    let publisher_topic = format!("{}-pub", gdp_name_to_string(topic_gdp_name));
+    let subscriber_topic = format!("{}-sub", gdp_name_to_string(topic_gdp_name));
+    add_entity_to_database_as_transaction("redis://127.0.0.1", &publisher_topic, gdp_name_to_string(publisher_listening_gdp_name).as_str()).expect("Cannot add publisher to database");
+    info!("publisher {} added to database with topic {}", gdp_name_to_string(publisher_listening_gdp_name), publisher_topic);
+    let subscribers = get_entity_from_database("redis://127.0.0.1", &subscriber_topic).expect("Cannot get subscriber from database");
+    
+    // signaling url is  [topic_name]/[local name]/[remote name]
+    // dialing url is [topic_name]/[remote name]/[local name]
+    for subscriber in subscribers {
+        info!("subscriber {}", subscriber);
+        let signaling_url = format!("{}/{}/{}", topic_name, gdp_name_to_string(publisher_listening_gdp_name), subscriber);
+        let dialing_url = format!("{}/{}/{}", topic_name, subscriber, gdp_name_to_string(publisher_listening_gdp_name));
+        let webrtc_stream = register_webrtc_stream(signaling_url, Some(dialing_url)).await;
+        info!("publisher registered webrtc stream");
+        let _ros_handle = ros_topic_creator(
+            webrtc_stream,
+            format!("{}_{}", "ros_manager_node", rand::random::<u32>()),
+            topic_name.clone(),
+            topic_type.clone(),
+            "sub".to_string(),
+            certificate.clone(),
+        )
+        .await;
+    }
+    //todo: check on the subscriber topic 
+    
+
+    // let webrtc_stream = register_webrtc_stream(gdp_name_to_string(topic_gdp_name), None).await;
+    // info!("publisher registered webrtc stream");
+    // let _ros_handle = ros_topic_creator(
+    //     webrtc_stream,
+    //     format!("{}_{}", "ros_manager_node", rand::random::<u32>()),
+    //     topic_name.clone(),
+    //     topic_type,
+    //     "sub".to_string(),
+    //     certificate.clone(),
+    // )
+    // .await;
 }
 
 async fn create_new_remote_subscriber(
     topic_gdp_name: GDPName, topic_name: String,
     topic_type: String, certificate: Vec<u8>,
 ) {
+    let subscriber_listening_gdp_name = generate_random_gdp_name();
 
-    let mut webrtc_stream = None;
-    loop {
-        let subscriber_listening_gdp_name = generate_random_gdp_name();
-        let result = timeout(Duration::from_secs(10), 
-            register_webrtc_stream(
-                gdp_name_to_string(subscriber_listening_gdp_name),
-                Some(gdp_name_to_string(topic_gdp_name)),
-            )
-        ).await;
-
-        match result {
-            Ok(res) => {
-                info!("got results!");
-                webrtc_stream = Some(res);
-                break;
-            },
-            Err(_) => {
-                warn!("subscribing retry timeout, retrying...");
-            },
-        };
-
-        println!("Retrying...");
+    // currently open another synchronous connection for put and get
+    let publisher_topic = format!("{}-pub", gdp_name_to_string(topic_gdp_name));
+    let subscriber_topic = format!("{}-sub", gdp_name_to_string(topic_gdp_name));
+    add_entity_to_database_as_transaction("127.0.0.1", &subscriber_topic, gdp_name_to_string(subscriber_listening_gdp_name).as_str()).expect("Cannot add publisher to database");
+    info!("publisher added to database");
+    let publishers = get_entity_from_database("127.0.0.1", &publisher_topic).expect("Cannot get subscriber from database");
+    
+    // signaling url is  [topic_name]/[local name]/[remote name]
+    // dialing url is [topic_name]/[remote name]/[local name]
+    for publisher in publishers {
+        info!("publisher {}", publisher);
+        let dialing_url = format!("{}/{}/{}", topic_name, gdp_name_to_string(subscriber_listening_gdp_name), publisher);
+        let signaling_url = format!("{}/{}/{}", topic_name, publisher, gdp_name_to_string(subscriber_listening_gdp_name));
+        let webrtc_stream = register_webrtc_stream(signaling_url, Some(dialing_url)).await;
+        info!("subscriber registered webrtc stream");
+        let _ros_handle = ros_topic_creator(
+            webrtc_stream,
+            format!("{}_{}", "ros_manager_node", rand::random::<u32>()),
+            topic_name.clone(),
+            topic_type.clone(),
+            "pub".to_string(),
+            certificate.clone(),
+        )
+        .await;
     }
 
-    let webrtc_stream= webrtc_stream.unwrap();
+    let webrtc_stream = register_webrtc_stream(
+        gdp_name_to_string(subscriber_listening_gdp_name),
+        Some(gdp_name_to_string(topic_gdp_name)),
+    ).await;
+
+
 
     let _ros_handle = ros_topic_creator(
         webrtc_stream,
@@ -163,30 +221,6 @@ pub async fn ros_topic_manager() {
     // bookkeeping the status of ros topics
     let mut topic_status = HashMap::new();
     let _ros_topic_manager_gdp_name = generate_random_gdp_name();
-
-    let pubsub_con = client::pubsub_connect("127.0.0.1", 6379).await.expect("Cannot connect to Redis");
-    let topic = "__keyspace@0__:mykey".to_string();
-    let mut msgs = pubsub_con
-    .psubscribe(&topic)
-    .await
-    .expect("Cannot subscribe to topic");
-
-    // let topic = "test".to_string();
-    // let mut msgs = pubsub_con
-    // .subscribe(&topic)
-    // .await
-    // .expect("Cannot subscribe to topic");
-
-
-    while let Some(message) = msgs.next().await {
-        match message {
-            Ok(message) => info!("KVS {}", String::from_resp(message).unwrap()),
-            Err(e) => {
-                eprintln!("ERROR: {}", e);
-                break;
-            }
-        }
-    }
 
     // read certificate from file in config
     for topic in config.ros {
